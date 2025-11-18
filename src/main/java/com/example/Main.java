4a10f3b6b448697c085d3882bb2b529bff6ac8bf
lai2-24 <lai2-24@itismeucci.com>
2025-11-18 13:05:23 +0100

    continuo

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -14,10 +14,11 @@ public class Main {
         ServerSocket ss = new ServerSocket(port);
         System.out.println("Server in ascolto nella porta " + port);
         Socket s = ss.accept();
-
+        String zona = "";
+        String[] comando = {"", "", ""};
+        
         BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
         PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-        boolean success = false;
         do{
             String msg = in.readLine();
             try {
@@ -25,20 +26,89 @@ public class Main {
                 if(pin_inserito != pin){
                     out.println("ERROR");
                 }
-                else success = true;
             } catch (Exception e) {
+                out.println("ERROR");
             }
-        }
-        while(!success);
-        String cmd = in.readLine();
-        switch (out) {
-            case value:
-                
+            String cmd = in.readLine();
+        comando = cmd.split(" ", 3);
+        switch (comando[1]) {
+            case "g":
+                if(comando[2].equals("A") || comando[2].equals("B") || comando[2].equals("C") || comando[2].equals("D")){
+                    switch (comando[2]) {
+                        case "A":
+                            if(zona.equals("A"))
+                                out.println("OFF");
+                            else if(zona.equals("")){
+                                out.println("ON");
+                                zona = "A";
+                            }
+                            else{
+                                out.println("ALREADY");
+                            }
+                            break;
+                        case "B":
+                            if(zona.equals("B"))
+                                out.println("OFF");
+                            else if(zona.equals("")){
+                                out.println("ON");
+                                zona = "B";
+                            }
+                            else{
+                                out.println("ALREADY");
+                            }
+                            break;
+                        case "C":
+                            if(zona.equals("C"))
+                                out.println("OFF");
+                            else if(zona.equals("")){
+                                out.println("ON");
+                                zona = "C";
+                            }
+                            else{
+                                out.println("ALREADY");
+                            }
+                            break;
+                        case "D":
+                            if(zona.equals("D"))
+                                out.println("OFF");
+                            else if(zona.equals("")){
+                                out.println("ON");
+                                zona = "D";
+                            }
+                            else{
+                                out.println("ALREADY");
+                            }
+                            break;
+                        default:
+                            out.println("ERROR");
+                            break;
+                    }
+                }
+                else 
+                    out.println("ERROR");
                 break;
-        
+            case "STATUS":
+                if(zona.equals("")){
+                    out.println("OFF");
+                }
+                else{
+                    out.println("ON ");
+                    out.println(zona);
+                }
+                break;
+            case "DIS":
+                if(!zona.equals(""))
+                    zona = "";
+                else{
+                    out.println("ERROR");
+                }
+                break;
             default:
+                out.println("ERROR");
                 break;
         }
-
+        }
+        while(true);
+        
     }
 }
